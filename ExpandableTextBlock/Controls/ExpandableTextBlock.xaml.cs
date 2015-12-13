95468fdd67b366bd6b591b9d8454c3c844fd43c0
Damien Delaire <delairedd@gmail.com>
2015-12-13 11:02:21 +0100

    First Commit

---
+++ ExpandableTextBlock/Controls/ExpandableTextBlock.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ExpandableTextBlock.Controls
+{
+    public sealed partial class ExpandableTextBlock : UserControl
+    {
+
+        public ExpandableTextBlock()
+        {
+            this.InitializeComponent();
+        }
+        public static readonly DependencyProperty TextStyleProperty = DependencyProperty.Register(
+           "TextStyle", typeof(Style), typeof(ExpandableTextBlock), new PropertyMetadata(default(Style), OnTextStyleChanged));
+
+        public Style TextStyle
+        {
+            get { return (Style)GetValue(TextStyleProperty); }
+            set { SetValue(TextStyleProperty, value); }
+        }
+
+        private static void OnTextStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctl = (ExpandableTextBlock)d;
+            ctl.CommentTextBlock.SetValue(StyleProperty, (Style)e.NewValue);
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(ExpandableTextBlock), new PropertyMetadata(default(string), OnTextChanged));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public double MaxHeight { get; set; }
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctl = (ExpandableTextBlock)d;
+            ctl.CommentTextBlock.SetValue(TextBlock.TextProperty, (string)e.NewValue);
+            ctl.CommentTextBlock.SetValue(TextBlock.HeightProperty, Double.NaN);
+
+            ctl.CommentTextBlock.Measure(new Size(ctl.CommentTextBlock.Width, double.MaxValue));
+
+            double desiredheight = ctl.CommentTextBlock.DesiredSize.Height;
+            ctl.CommentTextBlock.SetValue(TextBlock.HeightProperty, (double)63);
+
+            if (desiredheight > (double)ctl.CommentTextBlock.GetValue(TextBlock.HeightProperty))
+            {
+                ctl.ExpandHint.SetValue(StackPanel.VisibilityProperty, Visibility.Visible);
+                ctl.MaxHeight = desiredheight;
+            }
+            else
+            {
+                ctl.ExpandHint.SetValue(StackPanel.VisibilityProperty, Visibility.Collapsed);
+            }
+
+            //Setting length of comments
+            var boundsWidth = Window.Current.Bounds.Width;
+            ctl.CommentTextBlock.SetValue(TextBlock.WidthProperty, boundsWidth);
+        }
+
+        public static readonly DependencyProperty CollapsedHeightProperty = DependencyProperty.Register(
+            "CollapsedHeight", typeof(double), typeof(ExpandableTextBlock), new PropertyMetadata(default(double), OnCollapsedHeightChanged));
+
+
+        public double CollapsedHeight
+        {
+            get { return (double)GetValue(CollapsedHeightProperty); }
+            set { SetValue(CollapsedHeightProperty, value); }
+        }
+
+        private static void OnCollapsedHeightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctl = (ExpandableTextBlock)d;
+            ctl.CollapsedHeight = (double)e.NewValue;
+        }
+
+
+        private void LayoutRoot_OnTap(object sender, TappedRoutedEventArgs tappedRoutedEventArgs)
+        {
+           if ((Visibility)this.ExpandHint.GetValue(StackPanel.VisibilityProperty) == Visibility.Visible)
+            {
+                //transition
+                this.CommentTextBlock.SetValue(TextBlock.HeightProperty, Double.NaN);
+
+                this.ExpandHint.SetValue(StackPanel.VisibilityProperty, Visibility.Collapsed);
+            }
+        }
+    }
+
+}
---
+++ ExpandableTextBlock/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ExpandableTextBlock
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public string Description { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lobortis libero vel justo sollicitudin auctor. Nulla blandit pulvinar augue ac feugiat. Ut eros diam, lacinia mollis laoreet at, aliquam et metus. Vivamus ac dictum urna. In gravida dapibus lacus, ut ullamcorper risus congue ac. Cras ac volutpat lorem, eu imperdiet urna. Etiam volutpat venenatis magna. Vestibulum a tellus lobortis, varius nibh ut, molestie metus. Nunc tincidunt quis urna vel feugiat. Aenean placerat ut augue pharetra pharetra. Nam metus eros, pulvinar quis egestas ut, varius ac dui. Pellentesque eros arcu, tristique non sem quis, sodales congue magna. Cras eleifend porttitor lacus, et blandit lacus imperdiet id. Fusce id eros sed nulla consequat tempus.Ut commodo, ligula sed pretium mollis, sapien purus pulvinar purus, vel viverra sem sem sed tortor.Sed sagittis rhoncus diam. Suspendisse eget suscipit magna, nec auctor tortor. Nunc blandit congue tellus quis feugiat. Sed dapibus semper auctor. Nam pretium tempus mi quis posuere. Curabitur felis eros, pellentesque vel aliquam eget, hendrerit in orci.Morbi ut rhoncus massa. Curabitur fringilla orci id est pharetra vestibulum.Vestibulum iaculis, eros et sodales scelerisque, ante dui interdum nisl, consectetur laoreet diam urna sit amet dolor. Suspendisse mattis dolor eu nulla volutpat, vitae accumsan sapien commodo.Vivamus id turpis sem.";
+        }
+    }
+}
