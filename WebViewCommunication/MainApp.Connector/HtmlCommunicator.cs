551ebd5296e5a953c76767da3593ac5b032b15ea
Damien Delaire <delairedd@gmail.com>
2016-06-09 10:54:11 -0800

    adding Webview Communicator

---
+++ WebViewCommunication/MainApp.Connector/HtmlCommunicator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.Foundation.Metadata;
+
+
+namespace MainApp.Connector
+{
+    [AllowForWeb]
+    public sealed class HtmlCommunicator
+    {
+        public void getHtmlSpecificEvent()
+        {
+          // do something else
+        }
+
+        public string getAppVersion()
+        {
+            PackageVersion version = Package.Current.Id.Version;
+            return String.Format("{0}.{1}.{2}.{3}",
+                                 version.Major, version.Minor, version.Build, version.Revision);
+        }
+    }
+}
---
+++ WebViewCommunication/MainApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MainApp.Connector;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MainApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            string src = "ms-appx-web:///HTMLPage.html";
+            this.HtmlWebView.Navigate(new Uri(src));
+        }
+
+        private HtmlCommunicator communicationWinRT = new HtmlCommunicator();
+
+
+        private void HtmlWebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            this.HtmlWebView.AddWebAllowedObject("SCObject", communicationWinRT);
+        }
+    }
+}
