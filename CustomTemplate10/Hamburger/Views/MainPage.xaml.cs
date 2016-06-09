e49bc3c4ff6b406d801886d4464aeb2177f9d581
Damien Delaire <delairedd@gmail.com>
2016-06-09 12:35:42 -0800

    Custom Template 10 headerpage

---
+++ CustomTemplate10/Hamburger/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sample.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Sample.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
