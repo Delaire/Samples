fb3449cf15cb33c5f4be6d9b979bb622f458260f
Damien Delaire <delairedd@gmail.com>
2017-11-28 10:21:51 +0100

    Using state triggers to change your ItemTemplate in a GridView or ListView [XAML][UWP]

---
+++ ItemTemplateStateTriggers/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ItemTemplateStateTriggers.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ItemTemplateStateTriggers
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var items = new List<ItemsDataModel>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                items.Add(new ItemsDataModel()
+                {
+                    Title = string.Format("Hello this is cat number {0} , give him a like if you like him", i),
+                    ImagineUri = new System.Uri("http://cdn1-www.cattime.com/assets/uploads/gallery/maine-coon-cats-and-kittens/maine-coon-cats-and-kittens-1.jpg")
+
+                });
+            }
+
+
+
+            MyDemoList.ItemsSource = items;
+        }
+    }
+}
---
+++ ItemTemplateStateTriggers/Model/ItemsDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemTemplateStateTriggers.Model
+{
+    public class ItemsDataModel
+    {
+        public string Title { get; set; }
+        public Uri ImagineUri { get; set; }
+    }
+}
